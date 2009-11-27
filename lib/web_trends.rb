b9487f1deb742a3bde92cb84d574604b6a7a14cb
Benjamin Stein <ben@mcommons.com>
2009-11-27 13:49:47 -0500

    stubs for data extraction api methods

--- lib/web_trends.rb
+++ lib/web_trends.rb
@@ -15,6 +15,8 @@ class WebTrends
     @verbose = options[:verbose] || false
   end
 
+
+  #Methods for the Data Collection API
   def get_visitor_identifier
     self.class.post("/#{@dcsid}/ids.svc", :body=>extra_params)['id']
   end
@@ -27,6 +29,32 @@ class WebTrends
       :'WT.co_f' => visitor_id
     }.merge(extra_params).merge(event)
     self.class.post("/#{@dcsid}/events.svc", :body=>body)
+  end
+  
+  #Methods for the Data Extraction API
+  def list_profiles
+    # /profiles returns a list of profiles.
+  end
+  def profile_summary(profile_id)
+    # /profiles/{profile ID} returns summary data for a profile, including the last time the profile was analyzed, so client applications can determine when to refresh data. The interval between updates is at least 15 minutes.
+  end
+  def list_reports_for_profile(profile_id)
+    # /profiles/{Profile ID}/reports lists the reports defined for a profile.
+  end
+  def get_report_definition(profile_id, report_id)
+    # /profiles/{Profile ID}/reports/{Report ID}/info returns the report definition for the given report ID.
+  end
+  def list_report_time_periods(profile_id)
+    # profiles/{Profile ID}/periods returns the time periods for a given report.
+  end
+  def get_report_data_for_period(profile_id, report_id, period)
+    # /profiles/{Profile ID}/reports/{Report ID}/?period={Period} returns the report data for a given period.
+  end
+  def list_templates(profile_id)
+    # /profiles/{Profile ID}/templates/ returns a list of the template definitions (and their report definitions) that are associated with the specified profile.
+  end
+  def get_template_definition(profile_id, template_id)
+    # /profiles/{Profile ID}/templates/{Template ID} returns the definition of the specified template and its associated report definitions.
   end
   
 private 
